ea12a76adab7075067f375a2ada48cd37c821b91
pcayetan <po.cayetanot@gmail.com>
2024-06-12 17:28:27 +0200

    Simple weather

--- scripts/modis.py
+++ scripts/modis.py
@@ -1,13 +1,85 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
+import warnings
 
 import numpy as np
+from meteostat import Daily, Point
+import pandas as pd
+import pymodis
 import rasterio
 from rasterio.warp import Resampling
 import rioxarray as rxr
+import tqdm
+import yaml
 import xarray as xr
 import math
 
+
+def dataflow_modis(start_date, end_date, tiles, raw_dir, processing_dir, output_dir):
+    tiles = "h14v03"
+    product = "MOD14A1.061"
+    
+    # Download data
+    download_modis(start_date, end_date, output_dir=raw_dir, tiles=tiles, product=product)
+
+    # Get all file names 
+    textfile_name = f"listfile{product}.txt"
+    raw_dir = os.path.join(raw_dir, tiles)
+    textfile_path = os.path.join(raw_dir, textfile_name)
+    hdf_files  = get_modis_hdf_filelist(textfile_path)
+
+
+    return
+
+
+def get_modis_hdf_filelist(textfile_path: str):
+    hdf_files = []
+    dir = os.path.dirname(textfile_path)
+    with open(textfile_path) as file:
+        for line in file:
+            line = line.strip()
+            if line.endswith(".hdf"):
+                hdf_files.append(os.path.join(dir, line))
+    return hdf_files
+    
+
+def download_modis(start_date, end_date, output_dir:str = "../data/modis/raw", tiles:str = "h14v03", path:str = "MOLT", product:str = "MOD14A1.061"):
+    """ Fetches all the modis files between specific dates
+    """
+    full_path = os.path.join(output_dir,tiles)
+
+    textfile_name = f"listfile{product}.txt"
+    final_output_dir = os.path.join(output_dir, tiles)
+    textfile_path = os.path.join(final_output_dir, textfile_name)
+    if os.path.exists(full_path):
+        return textfile_path
+    
+    # Create directory for our data
+    os.mkdir(full_path)
+
+    # Fetch credentials for modis
+    with open("../config.yaml") as file:
+        credentials = yaml.safe_load(file)
+    user = credentials["modis"]["API_USER"]
+    password = credentials["modis"]["API_PASSWORD"]
+    
+
+    # Setup API call
+    downloader = pymodis.downmodis.downModis(
+        destinationFolder=full_path,
+        user=user,
+        password=password,
+        today=start_date,
+        enddate=end_date,
+        product=product,
+        path=path,
+        tiles=tiles,
+    )
+
+    # Call
+    downloader.connect()
+    downloader.downloadsAllDay(allDays=False)
+    return textfile_path
 
 def extract_fire_mask(input_path: str, output_dir: str = "../data/modis/processing") -> tuple[xr.Dataset, list[datetime], str]:
     """
@@ -40,7 +112,7 @@ def extract_fire_mask(input_path: str, output_dir: str = "../data/modis/processi
     output_full_path = os.path.join(output_dir, output_filename)
     dataset.rio.to_raster(output_full_path)
 
-    return dataset, date_objects, output_full_path
+    return date_objects, output_full_path
 
 
 def get_coords_and_pixels(dataset: rasterio.io.DatasetReader) -> tuple[list[tuple[float, float]], list[tuple[int, int]]]:
@@ -73,7 +145,7 @@ def crop(input_path: str, bbox_coords: tuple[float, float, float, float], output
     Saves a cropped version of a MODIS image based on bounding box coordinates.
 
     Parameters:
-    input_path (str): The path to the input MODIS image.
+    input_path (str): The path to the input MODIS image.if not os.path.exists():
     bbox_coords (tuple[float, float, float, float]): Bounding box coordinates (minx, miny, maxx, maxy).
     output_dir (str, optional): The directory to save the cropped image. Defaults to "../data/modis".
 
@@ -112,7 +184,7 @@ def crop(input_path: str, bbox_coords: tuple[float, float, float, float], output
     return output_full_path
     
     
-def resize(input_path: str, output_dir: str = "../data/modis/processing") -> str:
+def resize(input_path: str, output_dir: str = "../data/modis/final") -> str:
     """
     Resizes a clipped MODIS image to a new width and height.
 
@@ -155,7 +227,8 @@ def resize(input_path: str, output_dir: str = "../data/modis/processing") -> str
 
     return output_full_path
 
-def get_tile(lat_geographic, lon_geographic) -> tuple[int, int, float, float]:
+
+def get_tile(lat_geographic: float, lon_geographic: float) -> tuple[int, int, float, float]:
     """
     Get the tile index and pixel coordinates of a given geographic coordinate.
 
@@ -179,4 +252,56 @@ def get_tile(lat_geographic, lon_geographic) -> tuple[int, int, float, float]:
     horizontal_tile = int((lon_tile + 180) / 10)
     line = -(lat_tile - 90 + vertical_tile * 10) * 120 - 0.5
     sample = (180 - horizontal_tile * 10 + lon_tile) * 120 - 0.5
-    return vertical_tile, horizontal_tile, round(line, 8), round(sample, 8)
+    tile = f"h{0:02}v{1:02}".format(vertical_tile, horizontal_tile)
+    return tile
+
+
+def get_weather(input_path: str, date_objects):
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
+    # Open the resized MODIS image
+    tiff_file = rxr.open_rasterio(input_path)
+
+    # Initialize the dictionary to store masks
+    mask_types = ['tavg', 'prcp', 'wdir', 'wspd']
+    masks = {mask_type: np.zeros_like(tiff_file, dtype=float) for mask_type in mask_types}
+
+    # Open the input raster dataset
+    dataset = rasterio.open(input_path)
+    profile = dataset.profile.copy()
+
+    # TODO: Vectorize operations
+    for k, date in enumerate(date_objects):
+        print(f"Processing data for date: {date}")
+
+        for i in tqdm.tqdm(range(dataset.read(k + 1).shape[0])):
+            for j in range(dataset.read(k + 1).shape[1]):
+                point = Point(j, i)
+
+                # Get daily data
+                data = Daily(point, date, date)
+                data = data.interpolate()
+                data = data.fetch()
+
+                # If data is not empty, assign values to the mask arrays
+                if not data.empty:
+                    for mask_type in mask_types:
+                        masks[mask_type][k, i, j] = data[mask_type]
+                else:
+                    for mask_type in mask_types:
+                        masks[mask_type][k, i, j] = np.nan
+
+    save_weather(masks, profile)
+    return masks
+
+
+def save_weather(data: dict, profile, output_dir: str = "../data/modis/final"):
+    profile.update(
+        {
+            "dtype": "float64",
+        }
+    )
+    for key in data.keys():
+        path = os.path.join(output_dir, key)
+        with rasterio.open(path, "w", **profile) as dst:
+            dst.write(data[key])
